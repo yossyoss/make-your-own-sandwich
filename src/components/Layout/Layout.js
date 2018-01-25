381e1e3bc71fe2041487eeaab26bdbfd2dd63439
yossyoss <yosi0679@gmail.com>
2018-01-26 00:33:34 +0200

    adding aux as hoc

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxx';
 
 const layout = (props) => (
     <Aux>
---
+++ src/hoc/Auxx.js
@@ -0,0 +1,6 @@
+// A higher-order component is an advanced technique in React for reusing component logic
+// in this scenario we allow the user to return multiple elements without any root element 
+
+const auxx = (props) => props.children;
+
+export default auxx;
