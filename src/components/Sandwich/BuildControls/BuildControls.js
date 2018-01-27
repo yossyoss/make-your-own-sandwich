ac90b7e6b01cbfbae859e352ede89b5773ff6e1a
yossyoss <yosi0679@gmail.com>
2018-01-27 23:36:42 +0200

    .

--- src/components/Sandwich/BuildControls/BuildControls.js
+++ src/components/Sandwich/BuildControls/BuildControls.js
@@ -10,7 +10,7 @@ const controls = [
 
 const BuildControls = (props) => (
     <div className={classes.BuildControls}>
-    <p>Current Price: <strong>{props.price.toFixeג(2)}</strong></p>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
             key={ctrl.label} 
