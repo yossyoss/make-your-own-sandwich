44fa8d066037e52f68c8e62e9429bfa128ae1ef8
yossyoss <yosi0679@gmail.com>
2018-01-29 10:31:36 +0200

    adding sideDrawer and finishing app

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Auxx';
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+        </Aux>
+    );
+}
+ 
+export default sideDrawer;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-        <NavigationItems />
+        {/* <div onClick={props.openToolbar}>MENU</div> */}
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
